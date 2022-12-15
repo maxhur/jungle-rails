1442914c5e5ef71f05f7fcff308bd9fbc31774a6
doublefriedtofu <hurdonghyuk@gmail.com>
2022-12-15 17:02:05 -0500

    Add HTTP Basic Authentication Added basic authentication via env files

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with :name => "Jungle", :password => "book" 
+  http_basic_authenticate_with name: ENV["name"], password: ENV["password"]
   def show
   end
 end
