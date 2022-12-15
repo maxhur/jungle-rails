c2504fdb451eb90198481c0795a900b43306f712
doublefriedtofu <hurdonghyuk@gmail.com>
2022-12-15 18:19:33 -0500

    Add total count of products and category added total counts of products in stock and counts of categories. Used bootstrap to change the color of the hyperlink to product list data

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["name"], password: ENV["password"]
   def show
+    @countProducts = Product.pluck(:quantity)
+
+    @countCategories = Product.pluck(:category_id)
+
   end
 end
