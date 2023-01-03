ead724949c0475a3b9a4691de91e4fb745a9504e
doublefriedtofu <hurdonghyuk@gmail.com>
2023-01-03 17:11:20 -0500

    Cypress Test to ensure certain number of products are on display Initialized with two products added before cypress testing started. Wrote a test to test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -17,4 +17,8 @@ describe("example to-do app", () => {
     cy.get(".products article").should("be.visible");
   });
   
+  it("There are 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+  
 });
